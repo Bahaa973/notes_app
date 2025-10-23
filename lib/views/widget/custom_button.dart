585e07886842e94c8c792961b6133f3ff98f4199
Bahaa973 <bahaamarstawe@gmail.com>
2025-10-24 02:28:55 +0300

    create custom button and finish notes bottom sheet

---
+++ lib/views/widget/custom_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: KPrimaryColor,
+          padding: const EdgeInsets.symmetric(horizontal: 160, vertical: 10),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        ),
+        child: const Text(
+          'Add',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ));
+  }
+}
