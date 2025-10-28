6db1fb45c02bf67a7efe3848d07406a830b2e777
Bahaa973 <bahaamarstawe@gmail.com>
2025-10-29 02:14:00 +0300

    trigger add note cubit

--- lib/views/widget/add_note_form.dart
+++ lib/views/widget/add_note_form.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/model/note_model.dart';
 import 'package:notes_app/views/widget/custom_button.dart';
 import 'package:notes_app/views/widget/custom_text_field.dart';
 
@@ -49,6 +52,12 @@ class _AddNoteFormState extends State<AddNoteForm> {
             onPressed: () {
               if (formKey.currentState!.validate()) {
                 formKey.currentState!.save();
+                NoteModel noteModel = NoteModel(
+                    title: title!,
+                    subTitle: subTitle!,
+                    date: DateTime.now().toString(),
+                    color: Colors.blue.value);
+                BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
               } else {
                 autovalidateMode = AutovalidateMode.always;
                 setState(() {});
