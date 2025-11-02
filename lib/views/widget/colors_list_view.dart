469c7e86e0d4f73002cc9704a085b5a03c5f9d4c
Bahaa973 <bahaamarstawe@gmail.com>
2025-11-02 18:50:01 +0300

    add colors to note done

--- lib/views/widget/colors_list_view.dart
+++ lib/views/widget/colors_list_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 
 class ColorItem extends StatelessWidget {
   const ColorItem({super.key, required this.isActive, required this.color});
@@ -34,11 +36,11 @@ class ColorsListView extends StatefulWidget {
 class _ColorsListViewState extends State<ColorsListView> {
   int currentIndex = 0;
   List<Color> colors = const [
+    Color(0xff482C3D),
     Color(0xffAC3931),
     Color(0xffE5D352),
     Color(0xffD9E76C),
     Color(0xff537D8D),
-    Color(0xff482C3D),
   ];
 
   @override
@@ -54,6 +56,7 @@ class _ColorsListViewState extends State<ColorsListView> {
               child: GestureDetector(
                 onTap: () {
                   currentIndex = index;
+                  BlocProvider.of<AddNoteCubit>(context).color = colors[index];
                   setState(() {});
                 },
                 child: ColorItem(
