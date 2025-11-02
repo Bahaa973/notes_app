866a88bbcbc0fb1181add67b50fd8f28b2933c28
Bahaa973 <bahaamarstawe@gmail.com>
2025-11-02 16:37:33 +0300

    create color list view part one

--- lib/views/widget/add_note_form.dart
+++ lib/views/widget/add_note_form.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/model/note_model.dart';
+import 'package:notes_app/views/widget/colors_list_view.dart';
 import 'package:notes_app/views/widget/custom_button.dart';
 import 'package:notes_app/views/widget/custom_text_field.dart';
 
@@ -46,6 +47,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
             hint: 'Content',
             maxLines: 5,
           ),
+          const ColorsListView(),
           const SizedBox(
             height: 32,
           ),
